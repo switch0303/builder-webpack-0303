1635a423860ee20ec2c8821fbd6d5d14afa52e0e
switch0303 <coci315@163.com>
2019-08-17 07:07:51 +0800

    test: webpack-base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'E:/geektime/geektime-webpack-course/demos/my-project/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'E:/geektime/geektime-webpack-course/demos/my-project/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
